26ae0adfbb0cb453f96bad8b7045cf4c05b0035f
hnagib19 <h.nagib18@gmail.com>
2025-11-06 21:47:08 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+import http from "http";
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(`Hello from Cloud Run! Path: ${req.url}\n`);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
