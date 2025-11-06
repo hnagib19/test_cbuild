a5e279d941a980aa0b7b6545ba40cdfb5a7a1220
hnagib <h.nagib18@gmail.com>
2025-11-06 22:29:59 +0100

    initial test build 3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(`Hello from Cloud Run! Path: ${req.url}\n`);
+  res.end(`Hello zaaaaaaaaaaaba w chta saba from Cloud Run! Path: ${req.url}\n`);
 });
 
 server.listen(port, () => {
